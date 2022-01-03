1dee13510c0521f5e92e9073006022cbdb9baf1d
Marta Molina <marta.molina@live.u-tad.com>
2022-01-03 14:56:43 +0100

    Starting with post requests

--- FirstSpringProyect/src/main/java/com/example/demo/student/StudentController.java
+++ FirstSpringProyect/src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -13,7 +11,7 @@ import java.util.List;
 /*API -> SERVICE -> DATA ACCESS*/
 //http://localhost:8080/api/v1/student
 @RestController
-@RequestMapping(path="api/v1/student")
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
 
@@ -22,9 +20,15 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
     }
 
 
--- FirstSpringProyect/src/main/java/com/example/demo/student/StudentService.java
+++ FirstSpringProyect/src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -18,9 +19,11 @@ public class StudentService {
         this.studentRepository =studentRepository;
     }
 
-
-    @GetMapping
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
     }
 }
